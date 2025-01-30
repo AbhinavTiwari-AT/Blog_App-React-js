2149e97717f93fecec0e32962239eb7003ffb8d2
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-01-30 15:40:06 +0530

    router

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+        <h1>this is signup page</h1>
+        <p>Welcome to signup pasignup</p>
+    </div>
+  )
+}
+
+export default Signup
