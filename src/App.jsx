a9b44621dd702dcbdb65f5c35bcfdd3a1be76493
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-01-30 15:35:15 +0530

    React Router added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Base from './Components/Base';
 import { Button } from 'reactstrap';
+import {BrowserRouter,Routes,Route} from 'react-router';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 
 function App() {
 
   return (
     <>
-       <Base>
-       <h1>This is base Components</h1>
-       <Button color='warning'>Click here</Button>
-      </Base>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/About" element={<About/>}/>      
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
